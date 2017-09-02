e4d7037cb3efdd88f5f8619f2dfa366e23555765
fangyuan1985 <546219618@qq.com>
2017-09-02 15:06:19 +0800

    add dnscn.py

---
+++ dnscn.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding=utf-8 -*-
+
+import dns.resolver
+domain = raw_input('Please input an domain: ')
+cname = dns.resolver.query(domain,'CNAME')
+for i in cname.response.answer:
+    for j in i.items:
+        print j.to_text()
+
+
