18795517b9caf6691d760bb60d062fd2d707853a
方圆 <546219618@qq.com>
2017-09-29 21:05:48 +0800

    add time60.py

---
+++ time60.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+class Time60(object):
+    def __init__(self,hr,min):
+        self.hr = hr
+        self.min = min
+    def __str__(self):
+        return '%d:%d' % (self.hr,self.min)
+
+    def __add__(self,other):
+        return self.__class__(self.hr + other.hr,self.min + other.min)
+    __repr__ = __str__
+    
+    def __iadd__(self,other):
+        self.hr += other.hr
+        self.min += other.min
+        return self
+
